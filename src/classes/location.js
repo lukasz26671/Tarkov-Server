7025d3e631dbba56a507ec5e3ffc9425b8e64282
ae_King <aclengen@gmail.com>
2022-01-26 06:03:54 -0500

    revised DeepCopy by replicator

--- src/classes/location.js
+++ src/classes/location.js
@@ -185,9 +185,117 @@ function FindIfItemIsAPreset(ID_TO_SEARCH) {
   if (foundPresetsList.length == 0) return null;
   return foundPresetsList[0];
 }
-function DeepCopy(obj) {
-  return JSON.parse(JSON.stringify(obj));
+
+/* START NEW DEEPCOPY CODE */
+
+function cloneOtherType(target) {
+  const constrFun = target.constructor;
+  switch (toRawType(target)) {
+      case "Boolean":
+      case "Number":
+      case "String":
+      case "Error":
+      case "Date":
+          return new constrFun(target);
+      case "RegExp":
+          return cloneReg(target);
+      case "Symbol":
+          return cloneSymbol(target);
+      case "Function":
+          return target;
+      default:
+          return null;
+  }
 }
+
+function toRawType (value) {
+let _toString = Object.prototype.toString;
+let str = _toString.call(value)
+return str.slice(8, -1)
+}
+
+function cloneSymbol(targe) {
+  return Object(Symbol.prototype.valueOf.call(targe));
+}
+
+function cloneReg(targe) {
+  const reFlags = /\w*$/;
+  const result = new targe.constructor(targe.source, reFlags.exec(targe));
+  result.lastIndex = targe.lastIndex;
+  return result;
+}
+
+function forEach(array, iteratee) {
+  let index = -1;
+  const length = array.length;
+  while (++index < length) {
+      iteratee(array[index], index);
+  }
+  return array;
+}
+
+// core function
+function DeepCopy(target, map = new WeakMap()) {
+  // clone primitive types
+  if (typeof target != "object" || target == null) {
+      return target;
+  }
+
+  const type = toRawType(target);
+  let cloneTarget = null;
+
+  if (map.get(target)) {
+      return map.get(target);
+  }
+  map.set(target, cloneTarget);
+
+  if (type != "Set" && type != "Map" && type != "Array" && type != "Object") {
+      return cloneOtherType(target)
+  }
+
+  // clone Set
+  if (type == "Set") {
+      cloneTarget = new Set();
+      target.forEach(value => {
+          cloneTarget.add(DeepCopy(value, map));
+      });
+      return cloneTarget;
+  }
+
+  // clone Map
+  if (type == "Map") {
+      cloneTarget = new Map();
+      target.forEach((value, key) => {
+          cloneTarget.set(key, DeepCopy(value, map));
+      });
+      return cloneTarget;
+  }
+
+  // clone Array
+  if (type == "Array") {
+      cloneTarget = new Array();
+      forEach(target, (value, index) => {
+        cloneTarget[index] = DeepCopy(value, map);
+      })
+  }
+
+  // clone normal Object
+  if (type == "Object") {
+      cloneTarget = new Object();
+      forEach(Object.keys(target), (key, index) => {
+        cloneTarget[key] = DeepCopy(target[key], map);
+      })
+  }
+
+  return cloneTarget;
+}
+
+/* END NEW DEEPCOPY CODE */
+
+/* function DeepCopy(obj) {
+  return JSON.parse(JSON.stringify(obj));
+} */
+
 function GetRarityMultiplier(rarity) {
   switch (rarity) {
     case "Not_exist":
