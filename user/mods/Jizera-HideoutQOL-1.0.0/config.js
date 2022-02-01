bdb22d8d11fa148ccb9d5511fed17aec43702fed
ae_King <aclengen@gmail.com>
2022-02-01 16:56:05 -0500

    we use 7zip around these parts

---
+++ user/mods/Jizera-HideoutQOL-1.0.0/config.js
@@ -0,0 +1,17 @@
+//config.js
+module.exports = {
+    "HideoutAreas":{
+        "UseCustomConstructionTime": true, //false: default, true: Use a custom timer for structure construction time
+        "SetConstructionTime": 5, //Time in seconds
+
+        "RemoveFuelRequirement": false //false: default, true: Remove fuel consumption
+    },
+    "HideoutProduction":{
+        "UseCustomProductionTime": true, //false: default, true: Use a custom timer for item production time
+        "SetProductionTime": 5 //Time in seconds
+    },
+    "HideoutScavcase":{
+        "UseCustomScavcaseTime": true, //false: default, true: Use a custom timer for scavcase time
+        "SetScavcaseTime": 5 //Time in seconds
+    }
+}
