651d9b1645d1a18c45be19ed6dd1e4e7641ec709
CQInmanis <cquezadaguitar@gmail.com>
2022-03-06 19:32:09 -0300

    Updated 2 mods to be memory edits, archived unused mods

--- user/mods/Antiqque-AmmoStats-1.1.1/src/modloader.js
+++ user/mods/Antiqque-AmmoStats-1.1.1/src/modloader.js
@@ -1,23 +1,29 @@
 exports.mod = (mod_data) => {
+	//converted to memory edits by CQInmanis
 	
     logger.logInfo("[MOD] Antiqque Ammo Stats");
 	
-	
 	// load cache files we need
-	let PathResolver = global.internal.path.resolve;
-	let items = global.fileIO.readParsed(PathResolver('user/cache/items.json'));
-	let locale_en = global.fileIO.readParsed(PathResolver('user/cache/locale_en.json'));
+	//let PathResolver = global.internal.path.resolve;
+	//let items = global.fileIO.readParsed(PathResolver('user/cache/items.json'));
+	//let locale_en = global.fileIO.readParsed(PathResolver('user/cache/locale_en.json'));
+	let items = utility.DeepCopy(_database.items);
+	let locale_en = utility.DeepCopy(_database.locales.global.en);	
 	
-	
+
+	/* 
 	for(let itemId in items.data){
 		var item = items.data[itemId];
+	*/
+	for(let itemId in items){
+		var item = items[itemId];
 		
 		if(item._parent == "5485a8684bdc2da71d8b4567"){
-			item_name = locale_en.templates[itemId].Name
-			bullet_damage = item._props.Damage
-			ammo_type = item._props.ammoType
-			buckshot_bullets = item._props.buckshotBullets
-			bullet_pen    = item._props.PenetrationPower
+			let item_name = locale_en.templates[itemId].Name
+			let bullet_damage = item._props.Damage
+			let ammo_type = item._props.ammoType
+			let buckshot_bullets = item._props.buckshotBullets
+			let bullet_pen    = item._props.PenetrationPower
 			
 			if(ammo_type == "buckshot"){
 				locale_en.templates[itemId].Name = item_name+"  [ D: "+bullet_damage+"x"+buckshot_bullets+" P: "+bullet_pen+" ]"
@@ -28,7 +34,10 @@ exports.mod = (mod_data) => {
 		}
 	}
 	
-	fileIO.write(PathResolver('user/cache/locale_en.json'), locale_en, true);
-	
+	//fileIO.write(PathResolver('user/cache/locale_en.json'), locale_en, true);
+	_database.locales.global.en = locale_en;
+	//5fc382c1016cce60e8341b20   test
+	//logger.logError(JSON.stringify(_database.locales.global.en.templates["5fc382c1016cce60e8341b20"], null, 2));
+	//logger.logError(JSON.stringify(locale_en.templates["5fc382c1016cce60e8341b20"], null, 2));
 	logger.logSuccess("[MOD] Antiqque Ammo Stats Applied");
 }
--- user/mods/Nehax-MagazineManiac-1.0.0/src/FastLoading.js
+++ user/mods/Nehax-MagazineManiac-1.0.0/src/FastLoading.js
@@ -1,14 +1,18 @@
 exports.mod = (mod_data) => {
     logger.logSuccess("[MOD] Magazine Maniac loading...");
     let config = mod_data.settings;
-    let db_globals = fileIO.readParsed(db.base.globals);
+    //let db_globals = fileIO.readParsed(db.base.globals);
+    let db_globals = _database.globals;
     // set loading to (default/multiplier) if enabled, use default value if not
     if (config.enable){
         logger.logSuccess("[MOD] Magazine Maniac - FastLoading: On - Applying speed multiplier.");  
         db_globals.config.BaseLoadTime = parseFloat((0.85/config.speed_multiplier).toFixed(2));
+        db_globals.config.BaseUnloadTime = parseFloat((0.6/config.speed_multiplier).toFixed(2));
     }else{
         logger.logSuccess("[MOD] Magazine Maniac - FastLoading: Off - Default loading speed.");
         db_globals.config.BaseLoadTime = 0.85;
+        db_globals.config.BaseUnloadTime = 0.3;
     }
     _database.globals.config.BaseLoadTime = db_globals.config.BaseLoadTime;
+    _database.globals.config.BaseUnloadTime = db_globals.config.BaseUnloadTime;
 }
