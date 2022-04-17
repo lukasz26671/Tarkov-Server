2dfd1b912b8854eb313d6802f6f1de0a59a9f80c
PARGSoft <pargsoft@hotmail.com>
2022-04-17 16:46:26 +0100

    add WIP Notifier Service

--- src/classes/notifier.js
+++ src/classes/notifier.js
@@ -1,4 +1,5 @@
 "use strict";
+const { Server } = require('./../../core/server/server')
 
 /*
 * NotifierService class maintains a queue of notifications which will be pushed upon notification 
@@ -54,39 +55,61 @@ class NotifierService {
 	async notificationWaitAsync(resp, sessionID) {
 		await new Promise(resolve => {
 			// Timeout after 15 seconds even if no messages have been received to keep the poll requests going.
-			setTimeout(function () {
+			setTimeout(function() {
 				resolve();
 			}, 15000);
 
-			setInterval(function () {
+			setInterval(function() {
 				if (notifier_f.handler.hasMessagesInQueue(sessionID)) {
 					resolve();
 				}
 			}, 300);
 		});
-
+	
 		let data = [];
-
+		
 		while (this.hasMessagesInQueue(sessionID)) {
 			let message = this.popMessageFromQueue(sessionID);
 			// Purposefully using default JSON stringify function here to avoid newline insertion
 			// since the client expects different messages to be split by the newline character.
 			data.push(fileIO.stringify(message, true));
 		}
-
+	
 		// If we timed out and don't have anything to send, just send a ping notification.
 		if (data.length == 0) {
 			data.push('{"type": "ping", "eventId": "ping"}');
 		}
-
+	
 		server.sendTextJson(resp, data.join('\n'));
 	}
+
+	static getServer(sessionID)
+    {
+        return `${Server.getHttpsUrl()}/notifierServer/get/${sessionID}`;
+    }
+
+    static getWebSocketServer(sessionID)
+    {
+        return `${Server.getHttpsUrl()}/notifierServer/getwebsocket/${sessionID}`;
+    }
+
+    static getChannel(sessionID)
+    {
+        return {
+            "server": Server.getHttpsUrl(),
+            "channel_id": sessionID,
+            "url": NotifierService.getServer(sessionID),
+            "notifierServer": NotifierService.getServer(sessionID),
+            "ws": NotifierService.getWebSocketServer(sessionID)
+        };
+    }
 }
 
 /* Creates a new notification of type "new_message" with the specified dialogueMessage object. */
 function createNewMessageNotification(dialogueMessage) {
-	return { type: "new_message", eventId: dialogueMessage._id, data: { "dialogId": dialogueMessage.uid, "message": dialogueMessage } };
+	return {type: "new_message", eventId: dialogueMessage._id, data : {"dialogId": dialogueMessage.uid, "message": dialogueMessage}};
 }
 
+module.exports.NotifierService = NotifierService;
 module.exports.handler = new NotifierService();
 module.exports.createNewMessageNotification = createNewMessageNotification;
