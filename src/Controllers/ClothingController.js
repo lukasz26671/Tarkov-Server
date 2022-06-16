fd7b03a833d0ebfe293fd9457e2ae5647425feb8
PARGSoft <pargsoft@hotmail.com>
2022-06-16 17:38:59 +0100

    start clothing controller to replace customization

---
+++ src/Controllers/ClothingController.js
@@ -0,0 +1,8 @@
+
+class ClothingController {
+
+}
+
+module.exports.ClothingController = ClothingController;
+module.exports.clothing = new ClothingController();
+global.Clothing = ClothingController;
