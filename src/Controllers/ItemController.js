9c465bfc64473ef1a619b65ef1f81cba5058fbb2
Paulov <pargsoft@hotmail.com>
2022-07-13 09:09:35 +0100

    MIGRATE: Preset stuff to ItemController

--- src/Controllers/ItemController.js
+++ src/Controllers/ItemController.js
@@ -7,6 +7,26 @@ const utility = require('../../core/util/utility');
 class ItemController
 {
     static tplLookup = {};
+    static presetLookup = {};
+    static instance = new ItemController();
+
+    constructor() {
+
+        const presets = Object.values(global._database.globals.ItemPresets);
+        const reverse = {};
+
+        for (const p of presets) {
+            let tpl = p._items[0]._tpl;
+
+            if (!(tpl in reverse)) {
+                reverse[tpl] = [];
+            }
+
+            reverse[tpl].push(p._id);
+        }
+
+        ItemController.presetLookup = reverse;
+    }
 
     static getDatabaseItems() {
         const dbItems = DatabaseController.getDatabase().items;
@@ -98,7 +118,7 @@ class ItemController
     }
 
     /**
-     * Determines whether the item is an Ammo Box by TemplateId
+     * Determines whether the item is a preset by TemplateId
      * @param {*} tpl 
      * @returns {boolean} true/false
      */
@@ -182,6 +202,111 @@ class ItemController
   
     return child_items;
   }
+
+  static hasPreset(templateId) {
+        
+    return templateId in ItemController.lookup;
+  }
+
+  static isPreset(id) {
+    return id in global._database.globals.ItemPresets;
+}
+
+static getPresets(templateId) {
+    if (!this.hasPreset(templateId)) {
+        return [];
+    }
+
+    const presets = [];
+    const ids = ItemController.presetLookup[templateId];
+
+    for (const id of ids) {
+        presets.push(global._database.globals.ItemPresets[id]);
+    }
+
+    return presets;
+}
+
+//returns items array corresponding to the preset.
+static getBuiltWeaponPreset(presetID) {
+    if (!ItemController.isPreset(presetID)) {
+        logger.logError("not a preset: " + presetID);
+        return [];
+    }
+    let foundP = utility.DeepCopy(global._database.globals.ItemPresets[presetID]);
+    logger.logError(`Found preset for ID ${presetID}: \n` + JSON.stringify(foundP, null, 2));
+
+    for (let item of foundP._items) {
+        let ogID = item._id;
+        //repair ID
+        item._id = utility.generateNewItemId();
+
+        // check for children whose parentId was the item's original ID
+        // and replace it with the new id
+        for (let iitem of foundP._items) {
+            if (iitem.parentId == ogID) {
+                iitem.parentId = item._id;
+            }
+        }
+    }
+    return foundP._items;
+}
+
+//gets a random preset from a given receiver id
+static getRandomPresetIdFromWeaponId(WepId) {
+    if (!ItemController.hasPreset(WepId)) {
+        return "";
+    }
+    let wepPresets = [];
+    wepPresets = ItemController.getPresets(WepId);
+
+    if (wepPresets.length > 0) {
+        //logger.logSuccess("Found following presets: "+JSON.stringify(wepPresets, null, 2));
+        return wepPresets[utility.getRandomInt(0, wepPresets.length - 1)]._id;
+    } else {
+        logger.logError(`No presets found for ${WepId}.`); //should never enter here but, just in case.
+        return "";
+    }
+}
+
+static getStandardPreset(templateId) {
+    if (!ItemController.hasPreset(templateId)) {
+        return false;
+    }
+
+    const allPresets = ItemController.getPresets(templateId);
+
+    for (const p of allPresets) {
+        if ("_encyclopedia" in p) {
+            return p;
+        }
+    }
+
+    return allPresets[0];
+}
+
+static getBaseItemTpl(presetId) {
+    if (ItemController.isPreset(presetId)) {
+        const preset = global._database.globals.ItemPresets[presetId];
+
+        for (const item of preset._items) {
+            if (preset._parent === item._id) {
+                return item._tpl;
+            }
+        }
+    }
+
+    return "";
+}
+
+static findPresetByParent(parentId) {
+    let presetIndex = global._database.globals.ItemPresets.findIndex(p => p._parent === parentId);
+    if (presetIndex) { 
+        return global._database.globals.ItemPresets[p]._id; 
+    }
+}
+
+  
 }
 
 module.exports.ItemController = ItemController;
